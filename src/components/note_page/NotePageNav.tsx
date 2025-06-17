8355542f87f854609df6a9a527bbd505e3fc10cc
IhsanAlhakim <ihsanalhakim04@gmail.com>
2025-06-17 14:44:48 +0700

    feat: notes count show 0 if user doesnt have note

--- src/components/note_page/NotePageNav.tsx
+++ src/components/note_page/NotePageNav.tsx
@@ -2,7 +2,7 @@ import { Notebook } from "lucide-react";
 import { useNotes } from "../../contexts/notes_context";
 
 export default function NotePageNav() {
-  const {notes} = useNotes()
+  const { notes } = useNotes();
   return (
     <nav className="w-[275px] min-w-[275px] border-r-2">
       <ul>
@@ -10,7 +10,9 @@ export default function NotePageNav() {
           <Notebook className="mr-5" />
           <h2 className="text-lg font-semibold">Notes</h2>
           <div className="w-[40px] h-[25px] bg-blue-600 flex items-center justify-center rounded-lg ml-auto">
-            <p className="text-sm font-semibold text-white">{notes?.length}</p>
+            <p className="text-sm font-semibold text-white">
+              {notes && notes?.length > 0 ? notes.length : 0}
+            </p>
           </div>
         </li>
       </ul>
