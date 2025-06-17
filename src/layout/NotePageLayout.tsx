61b4a1fc6000d247c601f8c54601ba76399a5fdf
IhsanAlhakim <ihsanalhakim04@gmail.com>
2025-06-17 10:16:01 +0700

    feat: use Navigate component to navigate user to login page instead of use useNavigate() hook

--- src/layout/NotePageLayout.tsx
+++ src/layout/NotePageLayout.tsx
@@ -1,16 +1,11 @@
-import { Outlet, useNavigate } from "react-router";
+import { Navigate, Outlet } from "react-router";
 import { useUser } from "../contexts/user_context";
 
 export default function NotesPageLayout() {
   
   const { loggedInUser } = useUser();
-  const navigate = useNavigate()
 
-  if (!loggedInUser) return (
-    <div>
-      <button onClick={() => navigate("/login")}>Login First</button>
-    </div>
-  )
-
+  if (!loggedInUser) return <Navigate to={"/login"}/>
+  
   return <Outlet />;
 }
