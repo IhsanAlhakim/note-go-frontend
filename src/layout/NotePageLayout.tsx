d7beeb0fad0a29c81d8845e9a2e713c7958ccba6
IhsanAlhakim <ihsanalhakim04@gmail.com>
2025-06-17 14:21:36 +0700

    feat: add unknown error toast notification and navigate to login page if unknown error occurred when fetch logged in user data. add LoaderCircleIcon as well for when loading user data

--- src/layout/NotePageLayout.tsx
+++ src/layout/NotePageLayout.tsx
@@ -4,11 +4,16 @@ import { User } from "../App";
 import { UserContext } from "../contexts/user_context";
 import { responseStatusOK } from "../errors/http_error";
 import { getUser } from "../network/user_api";
+import { useToast } from "../components/Toast";
+import { unknownError } from "../errors/unknown_error";
+import { LoaderCircleIcon } from "lucide-react";
 
 export default function NotesPageLayout() {
   const navigate = useNavigate();
 
   const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
+
+  const { showToast } = useToast();
 
   useEffect(() => {
     async function fetchLoggedInUser() {
@@ -20,7 +25,9 @@ export default function NotesPageLayout() {
         }
         setLoggedInUser(getUserAPIResponse.data);
       } catch (error) {
-        console.log(error);
+        console.error(error);
+        showToast(unknownError.message[0]);
+        navigate("/login");
       }
     }
 
@@ -35,7 +42,9 @@ export default function NotesPageLayout() {
           <Outlet />
         </UserContext.Provider>
       ) : (
-        ""
+        <div className="flex w-full min-h-dvh justify-center items-center">
+          <LoaderCircleIcon className="animate-spin w-16 h-16 text-blue-500" />
+        </div>
       )}
     </>
   );
