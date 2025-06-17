15cd9f5859617f88a9cfbf975fe955de771a2090
IhsanAlhakim <ihsanalhakim04@gmail.com>
2025-06-17 10:23:48 +0700

    feat: add notes count

--- src/components/note_page/NotePageNav.tsx
+++ src/components/note_page/NotePageNav.tsx
@@ -1,6 +1,8 @@
 import { Notebook } from "lucide-react";
+import { useNotes } from "../../contexts/notes_context";
 
 export default function NotePageNav() {
+  const {notes} = useNotes()
   return (
     <nav className="w-[275px] min-w-[275px] border-r-2">
       <ul>
@@ -8,7 +10,7 @@ export default function NotePageNav() {
           <Notebook className="mr-5" />
           <h2 className="text-lg font-semibold">Notes</h2>
           <div className="w-[40px] h-[25px] bg-blue-600 flex items-center justify-center rounded-lg ml-auto">
-            <p className="text-sm font-semibold text-white">100</p>
+            <p className="text-sm font-semibold text-white">{notes?.length}</p>
           </div>
         </li>
       </ul>
