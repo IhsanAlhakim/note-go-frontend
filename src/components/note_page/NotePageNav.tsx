35c44977d9a6f34a0747086d3bff903dbe824faa
IhsanAlhakim <ihsanalhakim04@gmail.com>
2025-06-28 21:46:50 +0700

    feat: update note page nav bar design

--- src/components/note_page/NotePageNav.tsx
+++ src/components/note_page/NotePageNav.tsx
@@ -4,13 +4,13 @@ import { useNotes } from "../../contexts/notes_context";
 export default function NotePageNav() {
   const { notes } = useNotes();
   return (
-    <nav className="w-[275px] min-w-[275px] border-r-2">
+    <nav className="w-[275px] min-w-[275px] bg-blue-600">
       <ul>
-        <li className="flex items-center h-[70px] p-4 border-b-2 hover:bg-blue-500 hover:text-white transition-all">
+        <li className="flex items-center h-[70px] p-4 transition-all text-white border-l-8 border-white">
           <Notebook className="mr-5" />
           <h2 className="text-lg font-semibold">Notes</h2>
-          <div className="w-[40px] h-[25px] bg-blue-600 flex items-center justify-center rounded-lg ml-auto">
-            <p className="text-sm font-semibold text-white">
+          <div className="w-[40px] h-[25px] bg-blue-400 flex items-center justify-center rounded-lg ml-auto">
+            <p className="text-sm font-semibold">
               {notes && notes?.length > 0 ? notes.length : 0}
             </p>
           </div>
